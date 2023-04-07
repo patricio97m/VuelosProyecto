4bcc77f7197f9150774ed4831edf32648bdeac62
patricio97m <patricio.97m@gmail.com>
2023-04-07 17:28:57 -0300

    Primer entregable

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customers/List
+        public ActionResult List()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Juan Carlos" },
+                new Customer { Id = 2, Name = "María Florencia" },
+                new Customer { Id = 3, Name = "Juan Ignacio" },
+                new Customer { Id = 4, Name = "Luis Alberto" }
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Customers = customers,
+            };
+            return View(viewModel);
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int id)
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Juan Carlos" },
+                new Customer { Id = 2, Name = "María Florencia" },
+                new Customer { Id = 3, Name = "Juan Ignacio" },
+                new Customer { Id = 4, Name = "Luis Alberto" }
+            };
+            var customer = customers.FirstOrDefault(c => c.Id == id);
+            return View(customer);
+        }
+    }
+}
--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,5 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -8,8 +11,49 @@ namespace Vidly.Controllers
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Titanic" };
-            return View(movie);
+            var movie = new List<Movie>
+            {
+                new Movie { Name = "Titanic" },
+                new Movie { Name = "Toy Story" },
+                new Movie { Name = "Viernes 13" },
+                new Movie { Name = "El Padrino" }
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movies = movie,
+            };
+            return View(viewModel);
+        }
+
+
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id= " + id);
+        }
+
+        //movies 
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}/sortBy={1}", pageIndex, sortBy));
+
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+
+        public ActionResult ByReleaseDate(int year, byte month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
